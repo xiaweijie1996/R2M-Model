ead5f9d9c81f84bf8bfab292fdd943065958ce3b
Varma <rahulgunturu@gmail.com>
2022-02-17 20:14:07 +0100

    Added Files "not_sc.py" and "Tariff_df.py"

---
+++ Working_file/Tariff_df.py
@@ -0,0 +1,35 @@
+import os
+import pandas as pd
+import numpy as np
+# User Input 
+customer_input = input("Enter Customer number (Index): ")
+P1 = r'..\Output_Data\EnergyFlux_data\con_gen_data'
+R1 = P1 + str(customer_input) + '.csv'
+
+data_df = pd.read_csv(R1)
+tariff_df = pd.read_csv(r'..\Input_Data\Tarifa_demo.csv')
+t1 = tariff_df['TARIFA 2.0 TD']    
+t1_np = t1.to_numpy()
+t1_df = pd.DataFrame(t1_np)
+t2 = tariff_df['TARIFA 3.0 TD']
+t2_np = t2.to_numpy()
+t2_df = pd.DataFrame(t2_np)
+data1_df = pd.concat([t1_df, t2_df, data_df], axis=1)
+
+r2 = data_df["consumption"]
+r2_np = r2.to_numpy()
+r3 = data_df['use_power_grid1']
+r3_np = r3.to_numpy()
+
+# Tariffs_3 = np.array(['P1','P2','P3','P4','P5','P6'])
+# Energy_term_3  = np.array([0.171, 0.147, 0.117, 0.097, 0.083, 0.077])
+# Energy_term_3  = np.array([0.387, 0.350, 0.319, 0.293, 0.266, 0.253])
+# cons_p = []
+# for i in Tariffs_3:
+#     cons_p6 = np.sum(r2[t2_np == i])
+#     cons_p.append(cons_p6)
+# cons_np = np.array(cons_p)/1000
+# tot = (cons_np * Energy_term_3)/12
+# print(tot)
+# sum1 = np.sum(tot)
+# print(sum1)
---
+++ Working_file/not_sc.py
@@ -0,0 +1,59 @@
+from Tariff_df import *
+import pandas as pd
+import numpy as np
+import os
+
+user = input("which type of customer are you: ",  )
+if user == "Business":
+    Tariffs_3      = np.array(['P1','P2','P3','P4','P5','P6'])
+    Energy_term_3  = np.array([0.387, 0.350, 0.319, 0.293, 0.266, 0.253]) # EUR/MWh
+    Power_term_3   = np.array([0.045671833, 0.033543392, 0.016257762, 0.013830986, 0.009228504, 0.005896482])   # Euro per kw per day
+    Billed_power_3 = np.array([18, 18, 18, 18, 18, 18])  ## in KW
+    cons_p = []
+    for i in Tariffs_3:
+        cons_p6 = np.sum(r2[t2_np == i])
+        cons_p.append(cons_p6)
+    cons_np = np.array(cons_p)/1000
+    tot_energy = np.sum(cons_np * Energy_term_3)/12
+    tot_power = np.sum(Power_term_3 * Billed_power_3)* 30
+    print("Energy Consumption per month in MWh : ", np.sum(cons_np)/12)
+    
+    cons_p = []
+    for i in Tariffs_3:
+        cons_p6 = np.sum(r2[t2_np == i])
+        cons_p.append(cons_p6)
+    cons_np = np.array(cons_p)/1000
+
+elif user == "Residential" or user =="Homeowner":
+    Tariffs_2 = np.array(['P1','P2','P3'])
+    Energy_term_2  = np.array([0.407, 0.320, 0.262])
+    Power_term_2   = np.array([0.081583562, 0.008432877])    # Euro per kw per day
+    Billed_power_2 = np.array([4, 4])   ## in KW
+    cons_p = []
+    for i in Tariffs_2:
+        cons_p6 = np.sum(r2[t1_np == i])
+        cons_p.append(cons_p6)
+    cons_np = np.array(cons_p)/1000
+    tot_energy = np.sum(cons_np * Energy_term_2)/(12*19)  # for each household
+    tot_power = (2/3)*(Power_term_2[0] * Billed_power_2[0])* 30 + (1/3)*(Power_term_2[1] * Billed_power_2[1])*30
+    print("Energy Consumption per month in KWh : ",np.sum(cons_np)/12)
+else:
+    print("Please enter a valid customer type")
+
+print("Invoiced Energy per month in Euro : ",tot_energy)
+print("Invoiced Power per month in Euro : ",tot_power)
+
+tax_electricity = 0.051127
+meter_rent  = 0.81*30      # in Euro per month
+em_service  = 2.06      # in Euro per month
+IVA = 0.21       # 21 Percent
+
+tot = (tot_energy + tot_power) * (1 + tax_electricity)
+bef_iva = tot + em_service + meter_rent
+tot_inv = bef_iva * (1 + IVA)
+#print("Invoice after tax in Euros: ", tot)
+
+#print("Metered Equipment rental: in Euro ",meter_rent)
+#print("Electrical emergency service in Eur : ",em_service)
+#print("Invoice before IVA: " ,bef_iva)
+#print("Total Invoice in Euros: ", tot_inv)
